ff45faa5edfbc947419eae1a7a5d6afd12577f81
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-30 08:43:05 +0530

    urls.py

--- web_chat/urls.py
+++ web_chat/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts_app.urls')),  # Include accounts_app URLs
     path('', include('chat_app.urls')),
 ]
