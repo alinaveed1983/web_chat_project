6ba4ad800f03cbf8a52d88bdc46f0abe9f9cb403
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-30 08:57:06 +0530

    urls.py

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create_chat_room, name='create_room'),
+    path('<str:room_name>/', views.chat_room, name='chat_room'),
+    path('history/<str:room_name>/', views.chat_history, name='chat_history'),
 ]
