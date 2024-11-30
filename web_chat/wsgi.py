f5ee14b8ecb8eb2e6167798baf7c2c66e81e8b9b
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-30 08:42:39 +0530

    wsgi.py

--- web_chat/wsgi.py
+++ web_chat/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_chat.settings')
 
+# Initialize the WSGI application
 application = get_wsgi_application()
+
+# Add WhiteNoise to serve static files
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), '../staticfiles'))
